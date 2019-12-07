25fdff752ce0411d079e5f95f89d543acd572683
Natalie Fetsch <nfetsch@uw.edu>
2019-12-07 11:52:40 -0800

    multiple people works! but everyone is afraid

--- test1.py
+++ test1.py
@@ -8,16 +8,21 @@ import cv2
 import sys
 
 
+# Defaults
+emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
+size = 10
+place = (0,0)
 
-print ("the script has the name %s" % (sys.argv[0]))
-arguments = len(sys.argv) - 1
 
-
-print ("the script is called with %i arguments" % (arguments))
-
-default = 'alietest.jpeg'
+# Default file if none are provided
+default = 'multiple2.jpeg'
 file = "face_and_emotion_detection2/test_images/" + default
 output = './output/' + default
+
+# Check for input (and output) arguments
+arguments = len(sys.argv) - 1
+print ("the script is called with %i arguments" % (arguments))
+
 if arguments == 1:
     file = sys.argv[1]
 elif arguments == 2:
@@ -25,70 +30,57 @@ elif arguments == 2:
     output = sys.argv[2]
 
 
-       
-emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
-
-
-size = 10
-place = (0,0)
-
-
-# Load the jpg file into a numpy array
+# Load the jpg file into a numpy array, find faces with HOG model
+# (not as good as CNN model)
 image = face_recognition.load_image_file("./" + file)
-
-# Find all the faces in the image using the default HOG-based model.
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
 face_locations = face_recognition.face_locations(image)
+
 
 print("I found {} face(s) in this photograph.\n\n".format(len(face_locations)))
 
+copyIm = Image.open('./' + file).copy()
+
+# Find all the faces & paste Joseph over them
 for face_location in face_locations:
     
     # Print the location of each face in this image
     top, right, bottom, left = face_location
-    #print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}\n\n".format(top, left, bottom, right))
-    
-    # You can access the actual face itself like this:
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
-    # pil_image.show()
     size = int((bottom - top) * 1.55)
     place = (int(left - .1 * size), int(top - .25 * size))
 
+    # Access actual face
+    face_image = image[top:bottom, left:right]
+    face_image = cv2.resize(image, (48,48))
+    face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
+    face_image = np.reshape(face_image, [1, face_image.shape[0], face_image.shape[1], 1])
+    model = load_model("./face_and_emotion_detection2/emotion_detector_models/model_v6_23.hdf5")
 
+    # find emotion of current face
+    predicted_class = np.argmax(model.predict(face_image))
+    label_map = dict((v,k) for k,v in emotion_dict.items())
+    # predicted_label = label_map[predicted_class]
+    # print(predicted_label)
 
-face_image = cv2.resize(image, (48,48))
-face_image = cv2.cvtColor(face_image, cv2.COLOR_BGR2GRAY)
-face_image = np.reshape(face_image, [1, face_image.shape[0], face_image.shape[1], 1])
-model = load_model("./face_and_emotion_detection2/emotion_detector_models/model_v6_23.hdf5")
+    # Get the correct Joseph picture
+    im1 = Image.open('./face_and_emotion_detection2/graphics/anger.png')
 
+    if predicted_class == 1:
+        im1 =Image.open('./face_and_emotion_detection2/graphics/disgust.png')
+    elif predicted_class == 2:
+        im1 =Image.open('./face_and_emotion_detection2/graphics/fear.png')
+    elif predicted_class == 3:
+        im1 =Image.open('./face_and_emotion_detection2/graphics/happy.png')
+    if predicted_class == 4:
+        im1 =Image.open('./face_and_emotion_detection2/graphics/neutral.png')
+    elif predicted_class == 5:
+        im1 =Image.open('./face_and_emotion_detection2/graphics/sad.png')
+    elif predicted_class == 6:
+        im1 =Image.open('./face_and_emotion_detection2/graphics/surprise.png')
 
-predicted_class = np.argmax(model.predict(face_image))
-label_map = dict((v,k) for k,v in emotion_dict.items())
-predicted_label = label_map[predicted_class]
+    # paste face
+    fml = im1.resize((size, size))
+    copyIm.paste(fml, place, fml)
 
-
-# TODO: open correct Joseph image
-im1 = Image.open('./face_and_emotion_detection2/graphics/anger.png')
-# emotion_dict= {'Angry': 0, 'Sad': 5, 'Neutral': 4, 'Disgust': 1, 'Surprise': 6, 'Fear': 2, 'Happy': 3}
-if predicted_class == 1:
-    im1 =Image.open('./face_and_emotion_detection2/graphics/disgust.png')
-elif predicted_class == 2:
-    im1 =Image.open('./face_and_emotion_detection2/graphics/fear.png')
-elif predicted_class == 3:
-    im1 =Image.open('./face_and_emotion_detection2/graphics/happy.png')
-if predicted_class == 4:
-    im1 =Image.open('./face_and_emotion_detection2/graphics/neutral.png')
-elif predicted_class == 5:
-    im1 =Image.open('./face_and_emotion_detection2/graphics/sad.png')
-elif predicted_class == 6:
-    im1 =Image.open('./face_and_emotion_detection2/graphics/surprise.png')
-
-copyIm = Image.open('./' + file).copy()
-
-fml = im1.resize((size, size))
-
-copyIm.paste(fml, place, fml)
+# SAVE :D
 copyIm.save(output)
 copyIm.show()
